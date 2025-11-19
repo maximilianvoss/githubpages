0ad45d7c4bfa36a8e096e59f931b7ec3beb128b5
voss <voss@adobe.com>
2025-11-19 15:22:13 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,9 @@ import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
-  // Use '/' for development, '/githubpages/' for production build
-  const base = command === 'serve' ? '/' : '/githubpages/';
-  
   return {
     plugins: [react(), basicSsl()],
-    base: base,
+    base: '/',
     server: {
       https: true,
       port: 3000,
--- vite.config.manual-ssl.js
+++ vite.config.manual-ssl.js
@@ -6,7 +6,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/githubpages/',
+  base: '/',
   server: {
     https: {
       key: fs.readFileSync(path.resolve(__dirname, 'localhost-key.pem')),
